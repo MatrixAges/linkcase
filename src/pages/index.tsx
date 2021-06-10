e20b36dcbd3a5b0c10ed34ea721dd02639605821
matrixAge <xiewendao123@foxmail.com>
2021-06-10 10:22:39 +0800

    ✅  [add] test_images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import styles from './index.less'
 
 const Index = () => {
-	return <div className={styles.test}>options page</div>
+	return <div className={styles._local}>options page</div>
 }
 
 export default Index
