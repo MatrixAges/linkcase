7928422b76ac66f2e4cf4f8bf8865c25b9c864b3
xiewendao <xiewendao123@foxmail.com>
2021-06-12 00:19:27 +0800

    👌 [fix] set body height and overflow for macos over scroll

--- config/extension.ts
+++ config/extension.ts
@@ -1,41 +1,49 @@
-export default {
-	name: 'Linkcase',
-	version: '0.0.1',
-	description: 'A better way to reuse your bookcase and favorites.',
-	minimumChromeVersion: '72',
-	popupUI: '@/chrome/popup',
-	contentScripts: [
-		{
-			matches: ['<all_urls>'],
-			entries: ['@/chrome/scripts']
-		}
-	],
-	optionsUI: {
-		page: '@/pages/index',
-		openInTab: true
-	},
-	background: {
-		scripts: ['@/chrome/background'],
-		persistent: true
-	},
-	extends: {
-		chrome_url_overrides: {
-			newtab: 'options.html'
+const is_dev = process.env.NODE_ENV === 'development'
+
+export default Object.assign(
+	{
+		name: 'Linkcase',
+		version: '0.0.1',
+		description: 'A better way to reuse your bookcase and favorites.',
+		minimumChromeVersion: '72',
+		popupUI: '@/chrome/popup',
+		contentScripts: [
+			{
+				matches: ['<all_urls>'],
+				entries: ['@/chrome/scripts']
+			}
+		],
+		background: {
+			scripts: ['@/chrome/background'],
+			persistent: true
 		},
-		homepage_url: 'https://github.com/MatrixAges/linkcase'
+		extends: {
+			chrome_url_overrides: {
+				newtab: 'options.html'
+			},
+			homepage_url: 'https://github.com/MatrixAges/linkcase'
+		},
+		icons: {
+			16: 'logo/linkcase@16_dark.png',
+			32: 'logo/linkcase@32_dark.png',
+			48: 'logo/linkcase@48_dark.png',
+			128: 'logo/linkcase@128_dark.png'
+		},
+		permissions: [
+			'http://*/*',
+			'https://*/*',
+			'activeTab',
+			'storage',
+			'unlimitedStorage',
+			'bookmarks'
+		]
 	},
-	icons: {
-		16: 'logo/linkcase@16_dark.png',
-		32: 'logo/linkcase@32_dark.png',
-		48: 'logo/linkcase@48_dark.png',
-		128: 'logo/linkcase@128_dark.png'
-	},
-	permissions: [
-		'http://*/*',
-		'https://*/*',
-		'activeTab',
-		'storage',
-		'unlimitedStorage',
-		'bookmarks'
-	]
-}
+	is_dev
+		? {
+				optionsUI: {
+					page: '@/pages/index',
+					openInTab: true
+				}
+		  }
+		: {}
+)
